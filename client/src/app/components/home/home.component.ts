85592e7bad9d6434fb66cbb683427bca6cd2788d
RumenMoysev <rumoysev@gmail.com>
2024-03-14 17:56:51 +0200

    Explore button redirect to /gallery

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+    constructor(private router: Router) {}
+
+    buttonRedirect(): void {
+        this.router.navigate(['/gallery'])
+    }
 }
