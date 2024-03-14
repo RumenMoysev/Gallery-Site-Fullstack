16952e66772280846b6ceadee0754f317119e5af
RumenMoysev <rumoysev@gmail.com>
2024-03-14 15:20:30 +0200

    Add paintings list component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaintingsListComponent } from './models/paintings/paintings-list/paintings-list.component';
 
 const routes: Routes = [];
+    { path: 'gallery', component: PaintingsListComponent}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/models/paintings/paintings.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaintingsListComponent } from './paintings-list/paintings-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaintingsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaintingsModule { }
