ed35c79d62525089b1e6113df8a8819b5415d4b1
RumenMoysev <rumoysev@gmail.com>
2024-03-14 15:21:11 +0200

    Fix routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './models/user/login/login.component';
+import { RegisterComponent } from './models/user/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 import { PaintingsListComponent } from './models/paintings/paintings-list/paintings-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'gallery', component: PaintingsListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
