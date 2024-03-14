60609ce701acfdc4b9f9a423399531bacde3b442
RumenMoysev <rumoysev@gmail.com>
2024-03-14 15:21:03 +0200

    Add imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './user/login/login.component';
-import { HeaderComponent } from './core/header/header.component';
+import { LoginComponent } from './models/user/login/login.component';
+import { HeaderComponent } from './models/core/header/header.component';
+import { RegisterComponent } from './models/user/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaintingsListComponent } from './models/paintings/paintings-list/paintings-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HeaderComponent,
+        RegisterComponent,
+        HomeComponent,
+        PaintingsListComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
