db551fd5b5d533444c9b2377a7590476cebbf45a
RumenMoysev <rumoysev@gmail.com>
2024-03-18 18:05:10 +0200

    add directives

--- client/src/app/models/paintings/paintings.module.ts
+++ client/src/app/models/paintings/paintings.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PaintingsListComponent } from './paintings-list/paintings-list.component';
 import { AddPaintingComponent } from './add-painting/add-painting.component';
 import { PaintingDetailsComponent } from './painting-details/painting-details.component';
+import { ImageUrlDirective } from './directives/image-url-directive.directive';
 
 @NgModule({
   declarations: [
     PaintingsListComponent,
     AddPaintingComponent,
-    PaintingDetailsComponent
+    PaintingDetailsComponent,
+    ImageUrlDirective
   ],
   imports: [
     CommonModule,
--- client/src/app/models/user/user.module.ts
+++ client/src/app/models/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { EmailDirectiveDirective } from './directives/email-directive.directive';
 
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    EmailDirectiveDirective
   ],
   imports: [
     CommonModule
