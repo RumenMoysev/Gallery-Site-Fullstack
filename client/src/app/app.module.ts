0b321c04476c47a7b57afcbe5c9f977fa7136d11
RumenMoysev <rumoysev@gmail.com>
2024-03-22 16:01:25 +0200

    Add appInterceptorProvider

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { HeaderComponent } from './models/core/header/header.component';;
 import { HomeComponent } from './components/home/home.component';
 import { UserModule } from './models/user/user.module';
 import { PaintingsModule } from './models/paintings/paintings.module';
+import { appInterceptorProvider } from './app.interceptor';
 
 @NgModule({
     declarations: [
@@ -16,11 +18,12 @@ import { PaintingsModule } from './models/paintings/paintings.module';
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         UserModule,
         PaintingsModule,
         AppRoutingModule
     ],
-    providers: [],
+    providers: [appInterceptorProvider],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
