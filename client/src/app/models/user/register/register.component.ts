09b51f998b0df073df24e80d8181ae40cda6e2b8
RumenMoysev <rumoysev@gmail.com>
2024-03-22 16:01:03 +0200

    Add register functionality

--- client/src/app/models/user/register/register.component.ts
+++ client/src/app/models/user/register/register.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
-  register(form: NgForm):void {
-    console.log(form.valid)
-  }
+    constructor(private userService: UserService, private router: Router) {}
+
+    register(form: NgForm): void {
+        if (form.valid) {
+            const {email, username, passwords: {password, repeatPassword}} = form.value
+            this.userService.register(email, username, password, repeatPassword).subscribe(() => this.router.navigate(['/']))
+        }
+    }
 }
