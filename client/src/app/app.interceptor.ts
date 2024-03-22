2e1e84b5f8b4059a8da294896b9418eebdd5d8ac
RumenMoysev <rumoysev@gmail.com>
2024-03-22 16:01:38 +0200

    Create appInterceptor

---
+++ client/src/app/app.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+class AppInterceptor implements HttpInterceptor {
+  API: string = 'api'
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(request.url.startsWith(this.API)) {
+      request = request.clone({
+        url: request.url.replace(this.API, 'http://localhost:3030'),
+        withCredentials: true
+      });
+    };
+
+    return next.handle(request);
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  useClass: AppInterceptor,
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+}
