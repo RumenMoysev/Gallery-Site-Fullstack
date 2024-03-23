27a929f0f0a0f560fa28d5b29dfb0749ef9d49be
RumenMoysev <rumoysev@gmail.com>
2024-03-23 13:33:01 +0200

    Added cookie-parser

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,7 @@ const SERVER_PORT = 3030
 const expressConfigurator = require('./configs/expressConfig.js')
 const setCORS = require('./middlewares/corsMiddleware.js')
 const dbConnect = require('./configs/dbConfig.js')
+const cookieParser = require('cookie-parser')
 
 dbConnect()
     .then(() => console.log('DB connected successfully'))
@@ -16,6 +17,7 @@ dbConnect()
 expressConfigurator(app)
 setCORS(app)
 
+app.use(cookieParser())
 app.use(routes)
 
 app.listen(SERVER_PORT, () => console.log(`REST server is listening on port: ${SERVER_PORT}`))
