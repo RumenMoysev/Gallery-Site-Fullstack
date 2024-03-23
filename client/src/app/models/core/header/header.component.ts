2ca2cd5570f168bdf26b38187486ba8ef9515a0c
RumenMoysev <rumoysev@gmail.com>
2024-03-23 13:06:55 +0200

    Header is now reactive

--- client/src/app/models/core/header/header.component.ts
+++ client/src/app/models/core/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../user/user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent{
+    get isLoggedIn(): boolean {
+        return this.userService.isLoggedIn
+    }
 
+    constructor(private userService: UserService) { }
+
+    
 }
