f14f9c21c34bceb6caf9bac5fb1b720d64fa0fc2
RumenMoysev <rumoysev@gmail.com>
2024-03-28 20:49:37 +0200

    Create paintingsManagers.js

---
+++ server/src/managers/paintingsManagers.js
@@ -0,0 +1,33 @@
+const Painting = require('../models/Painting.js')
+
+const titleLength = 4
+const summaryLength = 10
+const descriptionLength = 20
+
+function validate (paintingData) {
+    if(paintingData.title.length < titleLength) {
+        throw new Error(`Painting title should be at least ${titleLength} characters`)
+    }
+
+    if (!paintingData.imageUrl.startsWith('http://') && !paintingData.imageUrl.startsWith('https://')) {
+        throw new Error('Please provie a valid image URL')
+    }
+
+    if (paintingData.summary.length < summaryLength) {
+        throw new Error(`Painting summary should be at least ${summaryLength} characters`)
+    }
+
+    if (paintingData.description.length < descriptionLength) {
+        throw new Error(`Painting description should be at least ${descriptionLength} characters`)
+    }
+}
+
+exports.createPainting = (paintingData) => {
+    try {
+        validate(paintingData)
+
+        return Painting.create(paintingData)
+    } catch (err) {
+        throw new Error(err.message)
+    }
+}
