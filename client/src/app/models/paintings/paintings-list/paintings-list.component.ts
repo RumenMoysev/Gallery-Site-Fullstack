5aa885b70e250f230c7dc1ad3ea32cb539bac86c
RumenMoysev <rumoysev@gmail.com>
2024-03-31 18:01:05 +0300

    Load all paintings

--- client/src/app/models/paintings/paintings-list/paintings-list.component.ts
+++ client/src/app/models/paintings/paintings-list/paintings-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaintingsService } from '../paintings.service';
+import { painting } from 'src/app/types/painting';
+import { tap } from 'rxjs';
 
 @Component({
-  selector: 'app-paintings-list',
-  templateUrl: './paintings-list.component.html',
-  styleUrls: ['./paintings-list.component.css']
+    selector: 'app-paintings-list',
+    templateUrl: './paintings-list.component.html',
+    styleUrls: ['./paintings-list.component.css']
 })
-export class PaintingsListComponent {
+export class PaintingsListComponent implements OnInit{
+    paintings: painting[] | undefined
 
+    constructor(private paintingsService: PaintingsService) {}
+
+    ngOnInit(): void {
+        this.paintingsService.getAllPaintings().pipe(tap(paintings => this.paintings = paintings as painting[])).subscribe()
+    }
 }
--- client/src/app/models/paintings/paintings.service.ts
+++ client/src/app/models/paintings/paintings.service.ts
@@ -6,7 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class PaintingsService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {}
+
+    getAllPaintings() {
+        return this.http.get('api/paintings')
+    }
 
     addPainting(title: string, summary: string, description: string, imageUrl: string) {
         const createdAtTime = new Date().toLocaleString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'shortOffset' })
