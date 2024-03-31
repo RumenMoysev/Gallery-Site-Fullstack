977676b0b16e37fe9da7237979ad3d9e31b1cbeb
RumenMoysev <rumoysev@gmail.com>
2024-03-31 18:01:27 +0300

    add router module

--- client/src/app/models/paintings/paintings.module.ts
+++ client/src/app/models/paintings/paintings.module.ts
@@ -5,6 +5,7 @@ import { AddPaintingComponent } from './add-painting/add-painting.component';
 import { PaintingDetailsComponent } from './painting-details/painting-details.component';
 import { ImageUrlDirective } from './directives/image-url-directive.directive';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaintingsModule { }
