21662eaaf37c442c734212694f0e5ffec5fd0835
RumenMoysev <rumoysev@gmail.com>
2024-04-01 18:22:41 +0300

    remove unused import

--- client/src/app/models/paintings/painting-details/painting-details.component.ts
+++ client/src/app/models/paintings/painting-details/painting-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaintingsService } from '../paintings.service';
 import { paintingDetails } from 'src/app/types/painting';
-import { tap } from 'rxjs';
 import { UserService } from '../../user/user.service';
 
 @Component({
