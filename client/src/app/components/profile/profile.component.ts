cc6a8cf6205ee384097b8c989405c42105bea712
RumenMoysev <rumoysev@gmail.com>
2024-04-03 16:42:45 +0300

    Completed profile functionality

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PaintingsService } from 'src/app/models/paintings/paintings.service';
 import { UserService } from 'src/app/models/user/user.service';
+import { painting } from 'src/app/types/painting';
 import { User } from 'src/app/types/user';
 
 @Component({
@@ -10,10 +11,27 @@ import { User } from 'src/app/types/user';
 })
 export class ProfileComponent implements OnInit {
   userData: User | undefined
+  paintings: painting[] | undefined
+
+  ownedSelected: boolean = true
+  likedSelected: boolean = false
 
   constructor(private userService: UserService, private paintingsService: PaintingsService) {}
 
   ngOnInit(): void {
     this.userService.getUser().subscribe(user => this.userData = user as User)
+    this.getOwned()
+  }
+
+  getOwned(): void {
+    this.paintingsService.getOwnedPaintings().subscribe(data => this.paintings = data as painting[])
+    this.ownedSelected = true
+    this.likedSelected = false
+  }
+
+  getLiked(): void {
+    this.paintingsService.getLikedPaintings().subscribe(data => this.paintings = data as painting[])
+    this.likedSelected = true
+    this.ownedSelected = false
   }
 }
--- client/src/app/models/paintings/paintings.service.ts
+++ client/src/app/models/paintings/paintings.service.ts
@@ -20,6 +20,14 @@ export class PaintingsService {
         return this.http.get(`api/paintings/${paintingId}`)
     }
 
+    getOwnedPaintings() {
+        return this.http.get(`api/users/getOwnedPaintings`)
+    }
+
+    getLikedPaintings() {
+        return this.http.get(`api/users/getLikedPaintings`)
+    }
+
     addPainting(title: string, summary: string, description: string, imageUrl: string) {
         const createdAtTime = new Date().toLocaleString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'shortOffset' })
         return this.http.post('api/paintings', {title, summary, description, imageUrl, createdAtTime})
