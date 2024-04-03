84db5a8597a36e9e071aed8c56e7b810c2d21986
RumenMoysev <rumoysev@gmail.com>
2024-04-03 23:44:44 +0300

    Auth guard now works even when you reload

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,17 +1,23 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../models/user/user.service';
+import { lastValueFrom } from 'rxjs';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = async () => {
   const userService = inject(UserService)
   const router = inject(Router)
+
+
+  if (!userService.isLoggedIn && document.cookie.includes('userId=')) {
+    console.log('gg')
+    await lastValueFrom(userService.getUser())
+  }
 
   return userService.isLoggedIn ? true : router.navigate(['/login'])
 };
 
 export const unauthGuard: CanActivateFn = () => {
   const userService = inject(UserService)
-  const router = inject(Router)
 
   return userService.isLoggedIn ? false : true
 };
