b1f3dbe91aa722c3504f563d1311bb19c233e0ca
RumenMoysev <rumoysev@gmail.com>
2024-04-11 18:23:40 +0300

    Improved auth and unauth guards

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,4 +1,4 @@
-import { inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../models/user/user.service';
 import { lastValueFrom } from 'rxjs';
@@ -6,18 +6,21 @@ import { lastValueFrom } from 'rxjs';
 export const authGuard: CanActivateFn = async () => {
   const userService = inject(UserService)
   const router = inject(Router)
-
-
-  if (!userService.isLoggedIn && document.cookie.includes('userId=')) {
-    console.log('gg')
-    await lastValueFrom(userService.getUser())
-  }
+  await waitForAuth(userService)
 
   return userService.isLoggedIn ? true : router.navigate(['/login'])
 };
 
-export const unauthGuard: CanActivateFn = () => {
+export const unauthGuard: CanActivateFn = async () => {
   const userService = inject(UserService)
+  const router = inject(Router)
+  await waitForAuth(userService)
 
-  return userService.isLoggedIn ? false : true
+  return userService.isLoggedIn ? router.navigate(['/']) : true
 };
+
+const waitForAuth = (userService: any): any => {
+  if (!userService.isLoggedIn && document.cookie.includes('userId=')) {
+    return lastValueFrom(userService.getUser())
+  }
+}
