76de8cc7352204b2697f257230d328994e3d18ca
RumenMoysev <rumoysev@gmail.com>
2024-05-14 19:37:37 +0300

    Changed urls

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -1,6 +1,12 @@
 function setCORS(app) {
     app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+        const allowedOrigins = ['http://192.168.0.121:4200', 'http://localhost:4200'];
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+
+        // res.setHeader('Access-Control-Allow-Origin', ['http://192.168.0.121:4200', 'http://localhost:4200']);
         res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.setHeader('Access-Control-Allow-Credentials', 'true')
